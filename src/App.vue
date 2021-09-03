466badbd5d5a380b0bac67af007e9faea38d5a03
OmojolaF <omojolaferanmi@gmail.com>
2021-09-03 11:29:12 +0100

    https://github.com/OmojolaF/login

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <div >
-    <div class="min-h-screen bg-purple-800 py-6 ">
+    <div class="min-h-screen bg-purple-light py-6">
       <div class="sm:mx-auto bg-white p-10 my-10 mx-5 rounded sm:py-10 max-w-screen-sm">
         <div class="max-w-md mx-auto">
-          <div class="text-3xl font-semibold py-3 my-2 text-center">
+          <div class="text-3xl font-normal py-3 my-2 text-center">
             <h1>Welcome Back!</h1>
           </div>
           <div class="placeholder-gray-800::placeholder">
             <form @submit="onSubmit">
-            <div class="py-3">
-              <input type="email" v-model="text" name="email" placeholder="Enter Your Email Address..." class="w-full p-2 pl-4 border shadow rounded-3xl focus:outline-none focus:ring-2 focus:ring-purple-600">
+            <div class="py-2">
+              <input type="email" v-model="text" name="email" placeholder="Enter Your Email Address..." class="w-full p-2 pl-4 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-purple-600 focus:bg-purple-100">
             </div>
-            <div class="py-3">
-              <input type="text" v-model="text" name="text" placeholder="Password" class="w-full p-2 pl-4 border shadow rounded-3xl focus:outline-none focus:ring-2 focus:ring-purple-600">
+            <div class="py-2">
+              <input type="text" v-model="text" name="text" placeholder="Password" class="w-full p-2 pl-4 border rounded-3xl focus:outline-none focus:ring-2 focus:ring-purple-600 focus:bg-purple-100">
             </div>
-            <div class="py-3">
+            <div class="py-2">
               <input type="checkbox" v-model="Remember" name="remember"/>
-              <label class="mx-2 font-normal">Remember Me</label>
+              <label class="mx-2 font-light text-purple">Remember Me</label>
             </div>
-            <input type="submit" value="LogIn" class="w-full py-3 px-6 my-2 mb-4 font-normal text-white bg-indigo-700 rounded-full hover:bg-yellow-300 hover:text-black"/>
-            <h5 class="border-t-2 border-gray-100 pt-2 text-center text-indigo-700 font-semibold hover:text-green-700"><a href="#">Forgot Password?</a></h5>
-            </form>  
+            </form> 
+            <button class="w-full my-2 py-2 px-5 font-normal text-white bg-purple-dark rounded-full hover:bg-yellow-300 hover:text-black">LogIn</button>
+            <hr class="mt-2 mb-2 border-gray-200">
+            <h5 class="text-center text-purple-dark font-semibold hover:text-green-700"><a href="#">Forgot Password?</a></h5> 
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          light: '#435c94',
+          DEFAULT: '#858796',
+          dark: '#4e73df',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
