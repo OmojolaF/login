e48f29d9ed78218aa7c2e1531b6ea49f1a051f49
OmojolaF <omojolaferanmi@gmail.com>
2021-09-05 13:45:55 +0100

    https://github.com/OmojolaF/login

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
             </form> 
             <button class="w-full my-2 py-3 px-5 font-normal text-sm text-white bg-purple-dark rounded-full hover:bg-purple-bright focus:outline-none focus:ring">Login</button>
             <hr class="mt-2 mb-2 border-gray-200">
-            <h5 class="text-center text-sm text-purple-dark py-3"><a href="#" class="leading-2 hover:text-purple-shine hover:underline">Forgot Password?</a></h5> 
+            <h5 class="text-center text-xs text-purple-dark py-3"><a href="#" class="hover:text-purple-shine hover:underline">Forgot Password?</a></h5> 
           </div>
         </div>
       </div>
