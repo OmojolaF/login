620ba474bba6ab9f8b9d0536d59b36b3857f0357
OmojolaF <omojolaferanmi@gmail.com>
2021-09-07 18:28:32 +0100

    https://github.com/OmojolaF/login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <!-- <div >
     <div class="min-h-screen bg-purple-light py-6">
       <div class="sm:mx-auto bg-white px-9 py-4 my-10 mx-5 rounded-md sm:px-14 sm:py-8 max-w-xl">
         <div class="max-w-md mx-auto">
@@ -25,11 +25,94 @@
         </div>
       </div>
   </div>
+  </div> -->
+
+    <div class="grid grid-cols-12 min-h-screen">
+    <div class="col-span-2 bg-purple-light py-2 px-3">
+      <div class="font-extrabold pb-4 pt-1">
+        <h1 class="text-white text-center">ORO LOUNGE <h2 class="text-white text-center text-xs">ADMIN</h2></h1>
+      </div>
+      <hr class="border-gray-400">
+      <a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+      </svg><h1 class="text-white text-sm inline-block py-4 font-extrabold">Dashboard</h1></a>
+      <hr class="border-gray-400 mb-2">
+      <h6 class="text-gray-400 text-xs font-extrabold py-4 px-2">RECORDS</h6>
+      <ul class="text-gray-300 text-sm px-2">
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+          </svg>Categories</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+          </svg>Caterings</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>Events</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+          </svg>Event Comments</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>Menus</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+          </svg>Newsletters</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+          </svg>Orders</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+          </svg>Testimonials</a></li>
+        <li class="py-4 hover:text-white"><a href="#" class=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block pr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>Users</a></li>
+      </ul>
+      <hr class="border-gray-400 mb-5">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 sm:mx-auto" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    <div class="col-span-10">
+      <div class="bg-white h-16 shadow-lg">
+        <div class="text-right"> 
+          <a href="" class="pr-3 text-sm">Ibukun Ajimoti
+            <img class="rounded-full h-12 w-12 my-auto inline-block ml-1" src="./assets/download.jpeg"/>
+          </a>
+        </div>
+      </div>
+      <div class="bg-gray-100 py-4 px-2 min-h-screen">
+        <ul class="">
+          <li>
+            <div class="">              
+              <input type="text" class="form-control" placeholder="Search for something">
+            </div>
+          </li>
+          <li class="">
+            <a href="./logout.vue" class=""><li class="py-3 transition duration-500 ease-in-out transform hover:translate-y-1"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+            </svg>Logout</li></a>
+          </li>
+        </ul>
+      </div>
+      <div class="justify-end">
+        <ul class="list-none">
+          <li><button>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 inline-block mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+            </svg>PREVIOUS</button>
+          </li>
+          <li><button>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 inline-block mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+            </svg>NEXT</button>
+          </li>
+        </ul>
+      </div>
+      <footer>
+        <div class="bg-white h-16 text-center my-auto text-sm">
+        Copyright © Oro Lounge - 2021
+        </div>
+      </footer>
+    </div>
   </div>
-
-
 </template>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
+import VueRouter from 'vue-router'
+import description from './components/description.vue'
+import contents from './components/contents.vue'
+import dashboard from './components/dashboard.vue'
+import forms from './components/forms.vue'
+import logout from './components/logout.vue'
+
+// Vue.use(VueRouter);
+
+// const routes = [
+//     {path: '/components/contents, component: contents'},
+//     {path: '/components/description, component: description'},
+//     {path: '/components/dashboard, component: dashboard'},
+//     {path: '/components/forms, component: forms'},
+//     {path: '/components/logout, component: logout'}
+// ];
+
+// const router = new VueRouter({
+//     routes,
+//     mode: 'history'
+// })
+
+// new Vue({
+//     el: '#app',
+//     router,
+//     render(h) {
+//         h(App)
+//     },
+// })
 
 createApp(App).mount('#app')
